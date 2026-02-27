af70c2c18c439ff841cd964fc5fa701d32363d3b
gryuewua <yuchenw7@nycstudents.net>
2026-02-27 09:11:47 -0500

    Create assesment.py

---
+++ assesment.py
@@ -0,0 +1,7 @@
+C = "C"
+. = "."
+
+N = int(input("How many spaces are there: "))
+yesterday = input("How were the spaces occupied yesterday: ").split()
+today = input("How were the spaces occupied today: ").split()
+
