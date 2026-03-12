83df3563cfdb249d988dc2f8256e378180c799fd
gryuewua <yuchenw7@nycstudents.net>
2026-03-12 09:14:09 -0400

    Update assesment.py

--- assesment.py
+++ assesment.py
@@ -28,7 +28,7 @@ language("The red cat sat on the mat. Why are you so sad cat? Don't ask that.") 
 
 'Assessment: Magnus'
 
-def honi(x):
+""" def honi(x):
     found=0
     y=0
     H=1
@@ -36,7 +36,7 @@ def honi(x):
     N=3
     for i in x:
         if (i == "H"):
-            y  = 1
+            y = 1
         if y == H:
             if (i == "O"):
                 H += 1
@@ -53,4 +53,39 @@ def honi(x):
                 
     print(found)
 
-honi('UAOSIDIOUAWIOUADWHWAOIDUWAOIDUNIFHOLYBUMBRONIHHHHOOOOLLLLYYYBUUUUMMMNIIII')
+honi('UAOSIDIOUAWIOUADWHWAOIDUWAOIDUNIFHOLYBUMBRONIHHHHOOOOLLLLYYYBUUUUMMMNIIII') """
+
+'Assessment: Gambling'
+
+def slots (quarters, m1, m2, m3):
+    plays = 0
+    m = 1
+    while quarters > 0:
+        if m == 1 and quarters > 0:
+            quarters -= 1
+            m1 += 1
+            plays += 1
+            m = 2
+            if m1 == 35:
+                quarters += 30
+                m1 = 0
+        if m == 2 and quarters > 0:
+            quarters -= 1
+            m2 += 1 
+            plays += 1
+            m = 3
+            if m2 == 100:
+                quarters += 60
+                m2 = 0
+        if m == 3 and quarters > 0:
+            quarters -= 1
+            m3 += 1
+            plays += 1
+            m = 1
+            if m3 == 10:
+                quarters += 9 
+                m3 = 0 
+    else:
+        print (f"Martha plays {plays} times before going broke.")
+
+slots (77, 4, 9, 3)
